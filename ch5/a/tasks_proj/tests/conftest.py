38876060ce5fb347f825920a32cf577f81000b27
Santosh Kumar <sntshkmr60@gmail.com>
2022-02-26 16:49:05 +0000

    Add --nice for motivation

--- ch5/a/tasks_proj/tests/conftest.py
+++ ch5/a/tasks_proj/tests/conftest.py
@@ -65,12 +65,21 @@ def db_with_multi_per_owner(tasks_db, tasks_mult_per_owner):
     for t in tasks_mult_per_owner:
         tasks.add(t)
 
-def pytest_report_header():
-    """Thank tester for running tests."""
-    return "Thanks for running the tests."
 
-def pytest_report_teststatus(report):
+def pytest_addoption(parser):
+    """Turn nice features on with --nice option."""
+    group = parser.getgroup('nice')
+    group.addoption("--nice", action="store_true", help="nice: turn failures into opportunities")
+
+
+def pytest_report_header(config):
+    """Thank tester for running tests."""
+    if config.getoption('nice'):
+        return "Thanks for running the tests."
+
+def pytest_report_teststatus(config, report):
     """Turn failures into opportunities."""
-    if report.when == 'call' and report.failed:
-        return (report.outcome, 'O', 'OPPORTUNITY for improvement')
+    if report.when == 'call':
+        if report.failed and config.getoption('nice'):
+            return (report.outcome, 'O', 'OPPORTUNITY for improvement')
 
